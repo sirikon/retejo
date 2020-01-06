999a4743e3ee8bb85a6eb228c8c7141999361c34
Carlos Fdez. Llamas <hello@sirikon.me>
2020-01-06 16:48:42 +0100

    Change var name

--- example/main.ts
+++ example/main.ts
@@ -7,10 +7,10 @@ const app = new App();
 app.route((router, nest, controller) => {
 
 	nest('/admin', router, (r) => {
-		r.get('/:name', controller(HomeController, (h) => h.handle));
+		r.get('/:name', controller(HomeController, (c) => c.handle));
 	});
 
-	router.get('/:name', controller(HomeController, (h) => h.handle));
+	router.get('/:name', controller(HomeController, (c) => c.handle));
 
 });
 
